ecbc2cb358e25d3ecd0800083d25e6860acecc32
FND <fnd@localhost.localdomain>
2018-03-09 14:28:50 +0100

    restructured components
    * sub-components now reside within the corresponding parent component's
      directory as transitive dependencies to improve encapsulation
    * moved sub-components' markup into separate macros
    
    essentially, the idea is for a macro to not rely on styling from a
    separate component (previously layout provided the markup abstraction
    while other components supplied the corresponding CSS, all managed in
    one big `index.scss`)

---
+++ components/layout/macros/footer.jsx
@@ -0,0 +1,14 @@
+import { assetURI } from "../../../views/util";
+import { createElement } from "complate-stream";
+
+let logo = assetURI("faucet-logotype-monochrome.svg");
+
+export default function Footer({ shortName }, ...links) {
+	return <footer class="site-footer">
+		<img src={logo} alt={shortName} class="logo" />
+
+		<nav>
+			{links}
+		</nav>
+	</footer>;
+}
