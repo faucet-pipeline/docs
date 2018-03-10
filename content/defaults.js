f1e62dac43f9ad2c15e800bff1f9cf0ef7985481
FND <fnd@localhost.localdomain>
2018-03-10 13:11:19 +0100

    refactored metadata handling
    * centralized common metadata - essentially, this means most pages will
      only need to provide a page title
    * document title is now composed of page title and project name by
      default (cf. Heydon's Inclusive Design Patterns book)
    * claim is used for meta description
    * the tagline's use of non-breaking spaces ensures more pleasant line
      breaks for most viewport widths

---
+++ content/defaults.js
@@ -0,0 +1,4 @@
+export let name = "faucet-pipeline";
+export let shortName = "faucet";
+export let tagline = "no-nonsense asset&nbsp;pipeline for&nbsp;humans";
+export let claim = `${name} – ${tagline.replace(/&nbsp;/g, " ")}`;
