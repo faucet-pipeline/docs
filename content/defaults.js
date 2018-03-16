ac302cfc963e03144df2580607ed3d329d475349
FND <fnd@localhost.localdomain>
2018-03-16 17:14:48 +0100

    restored original tagline
    @cdent convinced me that this is better than the alternative

--- content/defaults.js
+++ content/defaults.js
@@ -1,4 +1,4 @@
 export let name = "faucet-pipeline";
 export let shortName = "faucet";
-export let tagline = "no-nonsense asset&nbsp;pipeline for&nbsp;humans";
-export let claim = `${name} – ${tagline.replace(/&nbsp;/g, " ")}`;
+export let tagline = "The no-nonsense asset&nbsp;pipeline for&nbsp;humans";
+export let claim = `${name} – ${tagline.replace(/&nbsp;/g, " ").toLowerCase()}`;
